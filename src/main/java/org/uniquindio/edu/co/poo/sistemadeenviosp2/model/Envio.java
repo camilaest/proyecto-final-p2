d47c71ca7ec397d695b2265d2d865fbddd5e9bec
Talius31416 <tomixplayer07@gmail.com>
2025-10-21 22:19:43 -0500

    Update Envio.java

--- src/main/java/org/uniquindio/edu/co/poo/sistemadeenviosp2/model/Envio.java
+++ src/main/java/org/uniquindio/edu/co/poo/sistemadeenviosp2/model/Envio.java
@@ -6,7 +6,43 @@ public class Envio {
     private String direccionDestino;
     private long costo;
     private EstadoEnvio estadoEnvio;
+    public Envio(Builder builder) {
+        this.idEnvio = builder.idEnvio;
+        this.direccionOrigen = builder.direccionOrigen;
+        this.direccionDestino = builder.direccionDestino;
+        this.costo = builder.costo;
+        this.estadoEnvio = builder.estadoEnvio;
+    }
+    public static class Builder{
+        private String idEnvio;
+        private String direccionOrigen;
+        private String direccionDestino;
+        private long costo;
+        private EstadoEnvio estadoEnvio;
+        public Builder idEnvio(String idEnvio){
+            this.idEnvio = idEnvio;
+            return this;
+        }
+        public Builder direccionOrigen(String direccionOrigen){
+            this.direccionOrigen = direccionOrigen;
+            return this;
+        }
+        public Builder direccionDestino(String direccionDestino){
+            this.direccionDestino = direccionDestino;
+            return this;
+        }
+        public Builder costo(long costo){
+            this.costo = costo;
+            return this;
+        }
+        public Builder estadoEnvio(EstadoEnvio estadoEnvio){
+            this.estadoEnvio = estadoEnvio;
+            return this;
+        }
+        public Envio build(){
+            return new Envio(this);
+        }
 
-
+    }
 
 }
