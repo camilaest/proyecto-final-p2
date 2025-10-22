2b52ed787a1c9a04a28f6cb2bc9e196da03c035d
Talius31416 <tomixplayer07@gmail.com>
2025-10-21 22:20:04 -0500

    Update Repartidor.java

--- src/main/java/org/uniquindio/edu/co/poo/sistemadeenviosp2/model/Repartidor.java
+++ src/main/java/org/uniquindio/edu/co/poo/sistemadeenviosp2/model/Repartidor.java
@@ -1,4 +1,14 @@
 package org.uniquindio.edu.co.poo.sistemadeenviosp2.model;
 
-public class Repartidor {
+public class Repartidor extends Usuario{
+    private String idRepartidor;
+    public Repartidor(String nombreCompleto, String id, String telefono, String email, int edad, String usuario, String contraseña) {
+        super(nombreCompleto, id, telefono, email, edad, usuario, contraseña);
+    }
+    public String getIdRepartidor() {
+        return idRepartidor;
+    }
+    public void setIdRepartidor(String idRepartidor) {
+        this.idRepartidor = idRepartidor;
+    }
 }
